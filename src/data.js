ebb3861da4c4204cb4cb76bde5f27b7ef433568d
Coreykauai <corey@student.dev-island.org>
2024-02-20 20:37:15 -1000

    first commit

---
+++ src/data.js
@@ -0,0 +1,28 @@
+export const students = [
+  { name: "Corey", id: 1 },
+  { name: "Patrick", id: 2 },
+  { name: "Shea", id: 3 },
+  { name: "Kalsie", id: 4 },
+  { name: "Lilia", id: 5 },
+  { name: "Kayhla", id: 6 },
+];
+
+export const cats = [
+  { name: "Meowth", id: 11, age: 30, breed: "Pokemon" },
+  { name: "Tony T", id: 12, age: 60, breed: "Mascot" },
+  { name: "Garfeild", id: 13, age: 50, breed: "Cartoon" },
+  { name: "Tom", id: 14, age: 80, breed: "Cartoon" },
+  { name: "Cat", id: 15, age: 30, breed: "Cartoon" },
+  { name: "Gary", id: 16, age: 30, breed: "Snail" },
+];
+
+export const dogs = [
+  { name: "Miya", id: 111, age: 3, breed: "Shiba Inu" },
+  { name: "Kimi", id: 112, age: 7, breed: "Shiba Inu" },
+  { name: "Goofy", id: 113, age: 70, breed: "Cartoon Home Owner" },
+  { name: "Pluto", id: 114, age: 70, breed: "Cartoon Pet" },
+  { name: "Scooby", id: 115, age: 50, breed: "Cartoon Investigator" },
+  { name: "Snoopy", id: 116, age: 80, breed: "Beugal" },
+  { name: "Courage", id: 117, age: 30, breed: "Adopted" },
+  { name: "Balto", id: 118, age: 30, breed: "Wolf" },
+];
